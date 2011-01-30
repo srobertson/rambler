8dbc3bf609f1db810bb924563f3fcd5c79135225
Scott Robertson <scott.robertson@vibes.com>
2011-01-30 11:59:35 -0600

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rambler',
+    version = '2.0',
+    description='Framework for building Async applications',
+    author='Scott Robertson',
+    author_email='srobertson@codeit.com',
+    #package_dir = {'': 'src'},
+    packages = ['Rambler'],
+    #test_suite = 'your.module.tests',
+)
