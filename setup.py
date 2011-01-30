d87ac93785bb72c9e16c9fd72c857e98029af54a
Scott Robertson <scott.robertson@vibes.com>
2011-01-30 12:31:24 -0600

    adding dateutil dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author_email='srobertson@codeit.com',
     #package_dir = {'': 'src'},
     packages = ['Rambler'],
-    install_requires = ['zope.interface']
+    install_requires = ['zope.interface','dateutil']
     #test_suite = 'your.module.tests',
 )
