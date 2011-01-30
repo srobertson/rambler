36d1d6cdd14c1eb2fec8d00a4ca9fe5efb48aa16
Scott Robertson <scott.robertson@vibes.com>
2011-01-30 12:24:07 -0600

    adding rambler dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author_email='srobertson@codeit.com',
     #package_dir = {'': 'src'},
     packages = ['Rambler'],
-    requires = ['zope.interface', 'dateutil']
+    requires = ['zope.interface']
     #test_suite = 'your.module.tests',
 )
