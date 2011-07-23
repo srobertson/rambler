d2c05e031a55c39f7768d63af168ea6fa35b0666
Scott Robertson <srobertson@codeit.com>
2011-07-23 10:47:01 -0500

    added missing deps

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='srobertson@codeit.com',
     #package_dir = {'': 'src'},
     packages = ['Rambler','Rambler.controllers'],
-    install_requires = ['zope.interface','python-dateutil==1.5'],
+    install_requires = ['zope.interface','python-dateutil==1.5','yaml','pyparsing','pyMoney'],
     
     entry_points={
               'console_scripts': [
